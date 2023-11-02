066c73441a4477ede2c9177e42f6f897cd8e45df
nick16384 <119735278+nick16384@users.noreply.github.com>
2023-11-02 15:34:07 +0100

    Updated Monte Carlo Pi Python Script

--- CalculatePi_Monte_Carlo/CPU/CalculatePi.py
+++ CalculatePi_Monte_Carlo/CPU/CalculatePi.py
@@ -1,12 +1,17 @@
 from math import *
 
-radius = 10;
+radius = 100;
 inCircle = 0;
 total = radius * radius;
 
-for x in range(0, radius - 1):
-    for y in range(0, radius - 1):
-        if (sqrt(x*x+y*y) <= radius):
+# Note: range(a, b) includes a but excludes b
+for x in range(0, radius):
+    x = x + 0.5
+    print("x: ", x)
+    for y in range(0, radius):
+        y = y + 0.5
+        print("y: ", y)
+        if (x*x+y*y <= radius * radius):
             inCircle = inCircle + 1
 
-print(4 * inCircle / total)
+print(4*inCircle / total)
